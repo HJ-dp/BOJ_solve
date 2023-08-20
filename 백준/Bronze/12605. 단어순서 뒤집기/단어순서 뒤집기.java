271f7f3031b624aed551dfc482b965857195a506
HJ-dp <117727017+HJ-dp@users.noreply.github.com>
2023-08-20 23:15:02 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 108 ms, Memory: 12944 KB -BaekjoonHub

---
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i=1;i<=n;i++){
+            String s = sc.nextLine();
+            String[] arr = s.split(" ");
+            String[] answer = new String[arr.length];
+            for(int j=0;j<arr.length;j++){
+                answer[j] = arr[arr.length-1-j];
+            }
+            String a = String.join(" ",answer);
+            System.out.printf("Case #%d: %s\n",i,a);
+        }
+    }
+}

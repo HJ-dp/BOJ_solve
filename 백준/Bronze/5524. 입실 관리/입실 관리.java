9327e1eb11ac5375fad2945d5fa370199e67b06f
HJ-dp <117727017+HJ-dp@users.noreply.github.com>
2023-09-02 23:54:08 +0900

    [Bronze IV] Title: 입실 관리, Time: 120 ms, Memory: 12876 KB -BaekjoonHub

--- 백준/Bronze/5524. 입실 관리/입실 관리.java
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -1,1 +1,1 @@
-import java.util.*;public class Main {public static void main(String[] args) {Scanner sc = new Scanner(System.in);int N = sc.nextInt();for(int i=1;i<=N;i++) {System.out.println(sc.next().toLowerCase());}}}
+import java.util.*;public class Main {public static void main(String[] args) {Scanner sc=new Scanner(System.in);int N=sc.nextInt();for(int i=0;i<N;i++){System.out.println(sc.next().toLowerCase());}}}

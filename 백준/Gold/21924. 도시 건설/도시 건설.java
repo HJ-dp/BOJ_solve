25216125e041c744b252b81492007474b5e585d8
HJ-dp <117727017+HJ-dp@users.noreply.github.com>
2023-09-30 02:25:10 +0900

    [Gold IV] Title: 도시 건설, Time: 1256 ms, Memory: 237876 KB -BaekjoonHub

---
+++ 백준/Gold/21924. 도시 건설/도시 건설.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static int cnt, uf[];
+	public static long total, answer;
+	public static ArrayList<Node> arr;
+	public static boolean ok;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+			String[] temp = bf.readLine().split(" ");
+			int N = Integer.parseInt(temp[0]);
+			long M = Integer.parseInt(temp[1]);
+			arr = new ArrayList<>();
+			//메이크셋 
+			uf = new int[N+1];
+			for(int i=1;i<=N;i++) {
+				uf[i]= i;
+			}
+			
+			for(int i =0;i<M;i++) {
+				temp = bf.readLine().split(" ");
+				int a = Integer.parseInt(temp[0]);
+				int b = Integer.parseInt(temp[1]);
+				int c = Integer.parseInt(temp[2]);
+				total+=c;
+				arr.add(new Node(a,b,c));
+			}
+			
+			Collections.sort(arr);
+			for(Node n : arr) {
+				if(find(n.x) != find(n.y)) {
+					union(n.x,n.y);
+					cnt++;
+					answer+=n.d;
+				}
+				if(cnt==N-1) {
+					ok = true;
+					break;
+				}
+
+			}
+			if(ok){
+				sb.append(total-answer).append("\n");
+			} else {
+				sb.append(-1).append("\n");
+			}
+			System.out.println(sb);
+		
+	}// 메인 함수
+	
+	public static void union(int x,int y) {
+		int X = find(x);
+		int Y = find(y);
+		uf[X] = Y;
+	}
+	public static int find(int x) {
+		if(uf[x]==x) {
+			return x;
+		}
+		int root = find(uf[x]);
+		uf[x] = root;
+		return root;
+	}
+	static class Node implements Comparable<Node>{
+		int x;
+		int y;
+		int d;
+		public Node(int x,int y, int d) {
+			this.x = x;
+			this.y = y;
+			this.d = d;
+		}
+		
+		@Override
+		public int compareTo(Node n) {
+	        if(this.d > n.d) {
+	            return 1; 
+	        }else if (this.d==n.d){
+				return 0;
+			}
+	        return -1;
+	    }
+	}
+}

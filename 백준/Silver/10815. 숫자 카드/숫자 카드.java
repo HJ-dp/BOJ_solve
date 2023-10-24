ebd52e381c50c5f818f9d0a39ceea7d6d36504de
HJ <117727017+HJ-dp@users.noreply.github.com>
2023-10-24 13:36:02 +0900

    [Silver V] Title: 숫자 카드, Time: 3448 ms, Memory: 221592 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M, arr[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        //가지고 있는 카드
+        N = Integer.parseInt(bf.readLine());
+        arr = new int[N];
+        String[] temp = bf.readLine().split(" ");
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(temp[i]);
+        }
+        Arrays.sort(arr);
+        //찾을 숫자
+        M = Integer.parseInt(bf.readLine());
+        temp = bf.readLine().split(" ");
+        for(int i=0;i<M;i++){
+            int a = Integer.parseInt(temp[i]);
+            System.out.print(bs(a)+" ");
+        }
+        
+    }
+
+    public static int bs(int x){
+        int left = 0;
+        int right = N-1;
+        while(left <= right){
+            int mid = (left + right)/2;
+            if(arr[mid] > x){
+                right = mid -1;
+            } else if (arr[mid] < x){
+                left = mid + 1;
+            } else {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}

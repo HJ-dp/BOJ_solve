7978b6ffebaf9d7e6a3641fd7216ff73807ccad2
HJ <117727017+HJ-dp@users.noreply.github.com>
2023-11-02 03:20:10 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 684 ms, Memory: 67616 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static final int INF = Integer.MAX_VALUE;
+	public static int V, E, answer[], route[];
+    public static ArrayList<Node>[] arr;
+	public static StringBuilder sb = new StringBuilder();
+	public static PriorityQueue<Node> pq = new PriorityQueue<>();
+    public static boolean visited[];
+    public static Stack<Integer> st = new Stack<>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        //정점의 수
+        V = Integer.parseInt(bf.readLine());
+        //간선의 수
+		E = Integer.parseInt(bf.readLine());
+        //시작 정점
+        visited = new boolean[V+1];
+        answer = new int[V+1];
+        route = new int[V+1];
+        arr = new ArrayList[V+1];
+        for(int i=1;i<=V;i++){
+            arr[i] = new ArrayList<>();
+        }
+        while(E-->0){
+            String[] temp = bf.readLine().split(" ");
+            int a = Integer.parseInt(temp[0]);
+            int b = Integer.parseInt(temp[1]);
+            int c = Integer.parseInt(temp[2]);
+            arr[a].add(new Node(b,c));
+        }
+        String[] temp = bf.readLine().split(" ");
+        int a = Integer.parseInt(temp[0]);
+        int b = Integer.parseInt(temp[1]);
+        pq.add(new Node(a,0));
+        Arrays.fill(answer,INF);
+        answer[a] = 0;
+        route[a] = 0;
+        while(!pq.isEmpty()){
+            Node Num = pq.poll();
+            if(!visited[Num.n]){
+                visited[Num.n] =true;
+                for(Node i : arr[Num.n]){
+                    if(answer[i.n] > i.w + answer[Num.n]){
+                        answer[i.n] = i.w + answer[Num.n];
+                        route[i.n] = Num.n;
+                        pq.add(new Node(i.n,answer[i.n]));
+                    } 
+                }
+            }
+        }
+        sb.append(answer[b]).append("\n");
+        // st.push(b);
+        while(true){
+            st.push(b);
+            b = route[b];
+            if(b==0){
+                sb.append(st.size()).append("\n");
+                break;
+            }
+        }
+        while(st.size()!=0){
+            sb.append(st.pop()).append(" ");
+        }
+        System.out.println(sb);
+	}
+
+    static class Node implements Comparable<Node>{
+        int n, w;
+
+        public Node(int n, int w){
+            this.n = n;
+            this.w = w;
+        }
+        @Override
+        public int compareTo(Node n) {
+
+        if (this.w > n.w)
+            return 1;
+        else if (this.w < n.w)
+            return -1;
+        return 0;
+    }
+    }
+}
